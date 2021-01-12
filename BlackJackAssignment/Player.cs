1c6982d49cf9cda5c2c2d3d9b28788c4ebc3224b
kevin doherty <kevindoherty89@hotmail.com>
2021-01-12 17:29:43 +0000

    Added class methods correctly and added For statement

--- BlackJackAssignment/Player.cs
+++ BlackJackAssignment/Player.cs
@@ -20,26 +20,98 @@ namespace BlackJackAssignment
 
         }
 
-        public Player(string cardNumber, string cardSuite, int value, int total, string cardDealt)
-        {
-            CardDealt = cardDealt;
-            CardNumber = cardNumber;
-            CardSuite = cardSuite;
-            Value = value;
-            Total = total;
-        }
         public Player(string cardNumber, string cardSuite, int value)
         {
+            
             CardNumber = cardNumber;
             CardSuite = cardSuite;
             Value = value;
+           
         }
-      
+        public string GetCardNumber()
+        {
+            Random rand = new Random();
+
+            string[] _CardNumber = new string[12] { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "Jack", "Queen", "King" };
+            int cardNumber = rand.Next(_CardNumber.Length);
+            CardNumber = _CardNumber[cardNumber];
+            return CardNumber;
+        }
+        public string GetsCardSuite()
+        {
+            Random rand = new Random();
+            string[] _CardSuite = { "Hearts", "Spades", "Clubs", "Diamonds" };
+            int cardSuite = rand.Next(_CardSuite.Length);
+            CardSuite = _CardSuite[cardSuite];
+            return CardSuite;
+
+        }
+        public int CardValue()
+        {
+            int value = 0;
+
+
+            if (GetCardNumber() == "Jack")
+            {
+                value = 10;
+            }
+            if (GetCardNumber() == "Queen")
+            {
+                value = 10;
+            }
+            if (GetCardNumber() == "King")
+            {
+                value = 10;
+            }
+            if (GetCardNumber() == "2")
+            {
+                value = 2;
+            }
+            if (GetCardNumber() == "3")
+            {
+                value = 3;
+            }
+            if (GetCardNumber() == "4")
+            {
+                value = 4;
+            }
+            if (GetCardNumber() == "5")
+            {
+                value = 5;
+            }
+            if (GetCardNumber() == "6")
+            {
+                value = 6;
+            }
+            if (GetCardNumber() == "7")
+            {
+                value = 7;
+            }
+            if (GetCardNumber() == "8")
+            {
+                value = 8;
+            }
+            if (GetCardNumber() == "9")
+            {
+                value = 9;
+            }
+            if (GetCardNumber() == "Ace")
+            {
+                value = 11;
+            }
+
+            Value = value;
+
+
+            return Value;
+        }
 
 
 
-            
-        
+
+
+
+
 
     }
 }
--- BlackJackAssignment/Program.cs
+++ BlackJackAssignment/Program.cs
@@ -11,81 +11,21 @@ namespace BlackJackAssignment
         static void Main(string[] args)
         {
             Player p1 = new Player();
+            Player p2 = new Player();
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Card dealt is the {0} of {1}, value {2}", p1.GetCardNumber(), p1.GetsCardSuite(), p1.CardValue());
+            }
+
+           
+           
+
+            Console.ReadLine();
 
 
         }
-        public static string GetCardNumber()
-        {
-            Random rand = new Random();
-
-            string[] CardNumber = new string[12] { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "Jack", "Queen", "King" };
-            int cardNumber = rand.Next(CardNumber.Length);
-            return CardNumber[cardNumber];
-        }
-        public static string GetsCardSuite()
-        {
-            Random rand = new Random();
-            string[] CardSuite = { "Hearts", "Spades", "Clubs", "Diamonds" };
-            int cardSuite = rand.Next(CardSuite.Length);
-            return CardSuite[cardSuite];
-
-        }
-        public static int Value()
-        {
-            int value = 0;
-
-
-            if (GetCardNumber() == "Jack")
-            {
-                value = 10;
-            }
-            if (GetCardNumber() == "Queen")
-            {
-                value = 10;
-            }
-            if (GetCardNumber() == "King")
-            {
-                value = 10;
-            }
-            if (GetCardNumber() == "2")
-            {
-                value = 2;
-            }
-            if (GetCardNumber() == "3")
-            {
-                value = 3;
-            }
-            if (GetCardNumber() == "4")
-            {
-                value = 4;
-            }
-            if (GetCardNumber() == "5")
-            {
-                value = 5;
-            }
-            if (GetCardNumber() == "6")
-            {
-                value = 6;
-            }
-            if (GetCardNumber() == "7")
-            {
-                value = 7;
-            }
-            if (GetCardNumber() == "8")
-            {
-                value = 8;
-            }
-            if (GetCardNumber() == "9")
-            {
-                value = 9;
-            }
-            if (GetCardNumber() == "Ace")
-            {
-                value = 11;
-            }
-
-
-            return value;
-        }
+       
+        
     }
 }
