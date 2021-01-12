d035ba6054bd94743520b17dd00adfd1a855dacd
kevin doherty <kevindoherty89@hotmail.com>
2021-01-12 15:34:46 +0000

    Created Properties and Constructors

--- BlackJackAssignment/Player.cs
+++ BlackJackAssignment/Player.cs
@@ -8,5 +8,29 @@ namespace BlackJackAssignment
 {
     class Player
     {
+        public string CardDealt { get; set; }
+        public string CardNumber { get; set; }
+        public string CardSuite { get; set; }
+        public int Value { get; set; }
+        public int Total { get; set; }
+        public Player()
+        {
+
+        }
+
+        public Player(string cardNumber, string cardSuite, int value, int total, string cardDealt)
+        {
+            CardDealt = cardDealt;
+            CardNumber = cardNumber;
+            CardSuite = cardSuite;
+            Value = value;
+            Total = total;
+        }
+        public Player(string cardNumber, string cardSuite, int value)
+        {
+            CardNumber = cardNumber;
+            CardSuite = cardSuite;
+            Value = value;
+        }
     }
 }
