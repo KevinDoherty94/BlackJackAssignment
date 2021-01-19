13f83d0e8d6b1cae24db32e5f7afdb6e02ce83da
Kevin Doherty <kevindoherty1994@outlook.com>
2021-01-19 16:00:45 +0000

    Created correct class and got the randomisation working correctly

---
+++ BlackJackAssignment/DealHand.cs
@@ -0,0 +1,153 @@
+﻿namespace BlackJackAssignment
+{
+    using System;
+    using System.Threading;
+
+    /// <summary>
+    /// Defines the <see cref="DealHand" />.
+    /// </summary>
+    internal class DealHand
+    {
+        //Properties
+        /// <summary>
+        /// Gets or sets the CardDealt.
+        /// </summary>
+        public string CardDealt { get; set; }
+
+        /// <summary>
+        /// Gets or sets the CardNumber.
+        /// </summary>
+        public string CardNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the CardSuite.
+        /// </summary>
+        public string CardSuite { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Value.
+        /// </summary>
+        public int Value { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Total.
+        /// </summary>
+        public int Total { get; set; }
+
+        //Array which stores card face values
+        /// <summary>
+        /// Defines the cardFaceValues.
+        /// </summary>
+        private readonly string[] cardFaceValues = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "Jack", "Queen", "King" };
+
+        //Array that stores card suites
+        /// <summary>
+        /// Defines the cardSuite.
+        /// </summary>
+        private readonly string[] cardSuite = { "Hearts", "Spades", "Clubs", "Diamonds" };
+
+        //Constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DealHand"/> class.
+        /// </summary>
+        public DealHand()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DealHand"/> class.
+        /// </summary>
+        /// <param name="currentTotal">The currentTotal<see cref="int"/>.</param>
+        /// <param name="drawNumber">The drawNumber<see cref="int"/>.</param>
+        public DealHand(int currentTotal, int drawNumber)
+        {
+            OutputCardValuesToConsole(currentTotal, drawNumber);
+        }
+
+        //public DealHand(string cardNumber, string cardSuite, int value, int total, string cardDealt)
+        //{
+        //    CardDealt = cardDealt;
+        //    CardNumber = cardNumber;
+        //    CardSuite = cardSuite;
+        //    Value = value;
+        //    Total = total;
+        //}
+
+        //public DealHand(string cardNumber, string cardSuite, int value)
+        //{
+        //    CardNumber = cardNumber;
+        //    CardSuite = cardSuite;
+        //    Value = value;
+        //}
+
+        //Class methods
+        /// <summary>
+        /// The OutputCardValuesToConsole.
+        /// </summary>
+        /// <param name="currentTotal">The currentTotal<see cref="int"/>.</param>
+        /// <param name="drawNumber">The drawNumber<see cref="int"/>.</param>
+        public void OutputCardValuesToConsole(int currentTotal, int drawNumber)
+        {
+            int num1;
+            Total = 0;
+
+            for (int i = 0; i < drawNumber; i++)
+            {
+                // Added to stop dupication
+                Thread.Sleep(1);
+
+                Console.WriteLine("Card dealt is the {0} of {1}, value {2}", GetCardNumber(), GetsCardSuite(), num1 = GetValue());
+
+                Total += num1;
+            }
+
+            Total += currentTotal;
+        }
+
+        /// <summary>
+        /// The GetCardNumber.
+        /// </summary>
+        /// <returns>The <see cref="string"/>.</returns>
+        public string GetCardNumber()
+        {
+            Random rand = new Random();
+            CardNumber = cardFaceValues[rand.Next(0, cardFaceValues.Length)];
+
+            return CardNumber;
+        }
+
+        /// <summary>
+        /// The GetsCardSuite.
+        /// </summary>
+        /// <returns>The <see cref="string"/>.</returns>
+        public string GetsCardSuite()
+        {
+            Random rand = new Random();
+            CardSuite = cardSuite[rand.Next(0, cardSuite.Length)];
+
+            return CardSuite;
+        }
+
+        /// <summary>
+        /// The GetValue.
+        /// </summary>
+        /// <returns>The <see cref="int"/>.</returns>
+        public int GetValue()
+        {
+            // 2 - 9 numbers only lenght of 1, Ace, Jack and Queen are greater
+            if (CardNumber.Length == 1)
+            {
+                return Convert.ToInt16(CardNumber);
+            }
+
+            // Ace
+            if (CardNumber == cardFaceValues[0])
+            {
+                return 1;
+            }
+
+            // Jack, Queen or King
+            return 10;
+        }
+    }
+}
--- BlackJackAssignment/Program.cs
+++ BlackJackAssignment/Program.cs
@@ -1,26 +1,105 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BlackJackAssignment
+﻿namespace BlackJackAssignment
 {
-    class Program
+    using System;
+
+    /// <summary>
+    /// Defines the <see cref="Program" />.
+    /// </summary>
+    internal class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Defines the MaxValue.
+        /// </summary>
+        private const int MaxValue = 21;
+
+        /// <summary>
+        /// Gets or sets the DrawNumber.
+        /// </summary>
+        public static int DrawNumber { get; set; } = 2;
+
+        /// <summary>
+        /// Gets or sets the CurrentPlayerTotal.
+        /// </summary>
+        public static int CurrentPlayerTotal { get; set; }
+
+        /// <summary>
+        /// Gets or sets the StickOrTwist.
+        /// </summary>
+        public static string StickOrTwist { get; set; }
+
+        /// <summary>
+        /// The Main.
+        /// </summary>
+        /// <param name="args">The args<see cref="string[]"/>.</param>
+        private static void Main(string[] args)
         {
-            //Uses Player class to call a method which outputs a randomised card value
+            // Black Jack Flow
+            Console.WriteLine("Player Plays");
+            DealPlayerHand();
 
-            Player p1 = new Player();
-
-            p1.OutputConsole();
-
+            Console.WriteLine("Dealer Plays");
+            DealDealerHand();
         }
 
+        protected Program()
+        {
+        }
 
+        /// <summary>
+        /// The DealPlayerHand.
+        /// </summary>
+        private static void DealPlayerHand()
+        {
+            DealHand newHand = new DealHand(CurrentPlayerTotal, DrawNumber);
+            Console.WriteLine("Your score is {0}", newHand.Total);
 
-       
-        
+            if (newHand.Total < MaxValue)
+            {
+                Console.WriteLine("Do you want to stick or twist - s/t?");
+                StickOrTwist = Console.ReadLine();
+
+                if (StickOrTwist.ToLower() == "t")
+                {
+                    DrawNumber = 1;
+                    CurrentPlayerTotal = newHand.Total;
+                    DealPlayerHand();
+                }
+            }
+            else if (newHand.Total > MaxValue)
+            {
+                Console.WriteLine($"Bust! Score above {MaxValue}");
+            }
+
+            // reset
+            CurrentPlayerTotal = 0;
+            DrawNumber = 2;
+            Console.WriteLine(string.Empty);
+        }
+
+        /// <summary>
+        /// The DealDealerHand.
+        /// </summary>
+        private static void DealDealerHand()
+        {
+            DealHand newHand = new DealHand(CurrentPlayerTotal, DrawNumber);
+            Console.WriteLine("Dealer score is {0}", newHand.Total);
+
+            if (newHand.Total < MaxValue)
+            {
+                Console.WriteLine("Do you want to stick or twist - s/t?");
+                StickOrTwist = Console.ReadLine();
+
+                if (StickOrTwist.ToLower() == "t")
+                {
+                    DrawNumber = 1;
+                    CurrentPlayerTotal = newHand.Total;
+                    DealDealerHand();
+                }
+            }
+            else if (newHand.Total > MaxValue)
+            {
+                Console.WriteLine($"Bust! Score above {MaxValue}");
+            }
+        }
     }
 }
